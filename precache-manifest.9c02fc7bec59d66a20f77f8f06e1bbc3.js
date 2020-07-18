841d9df5021fd39b8dbf75d4b1f55e3a7748cea5
Tejas C Parkar <tparkar0@gmail.com>
2020-07-18 21:02:12 +0530

    Updates

---
+++ precache-manifest.9c02fc7bec59d66a20f77f8f06e1bbc3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d786e809f8ebe87fa6cf5b4c839142",
+    "url": "/amrapalifoils/index.html"
+  },
+  {
+    "revision": "2f9cd33e1a7f446469bc",
+    "url": "/amrapalifoils/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "98518fc714e815e45b05",
+    "url": "/amrapalifoils/static/js/2.07560b07.chunk.js"
+  },
+  {
+    "revision": "1bd0345857c7008a8ec0ed6e7d8f085c",
+    "url": "/amrapalifoils/static/js/2.07560b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f9cd33e1a7f446469bc",
+    "url": "/amrapalifoils/static/js/main.d2bc1263.chunk.js"
+  },
+  {
+    "revision": "1633883662687cc9f40d",
+    "url": "/amrapalifoils/static/js/runtime-main.59108786.js"
+  },
+  {
+    "revision": "748352d912ade3b14fe212885e3a98d7",
+    "url": "/amrapalifoils/static/media/slider-img-1.748352d9.jpg"
+  },
+  {
+    "revision": "643b0b048beb5ab36cb042c0522c2c93",
+    "url": "/amrapalifoils/static/media/slider-img-2.643b0b04.jpg"
+  },
+  {
+    "revision": "43b098ad10b7ca413bb0ca02cc16917e",
+    "url": "/amrapalifoils/static/media/slider-img-3.43b098ad.jpg"
+  }
+]);
